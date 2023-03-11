86adbc177152f09d294a6066d53b223893a933ba
achaudhary591 <achaudhary591@gmail.com>
2023-03-11 17:42:17 +0530

    added functionality to change price after removing data from cart

--- lib/widgets/cart_items.dart
+++ lib/widgets/cart_items.dart
@@ -1,31 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/providers.dart';
 
 class CartItems extends StatelessWidget {
-
   final String id;
   final double price;
   final int quantity;
   final String title;
+  final String productId;
 
-  CartItems({required this.id, required this.price, required this.quantity, required this.title});
+  CartItems({
+    required this.id,
+    required this.price,
+    required this.quantity,
+    required this.title,
+    required this.productId,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).colorScheme.errorContainer,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          title: Text(title),
-          subtitle: Text('Total: ₹${(price * quantity)}'),
-          trailing: Text('$quantity x'),
-          leading: CircleAvatar(
-            child: FittedBox(
-              child: Text(
-                '₹$price',
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId,);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            title: Text(title),
+            subtitle: Text('Total: ₹${(price * quantity)}'),
+            trailing: Text('$quantity x'),
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: FittedBox(
+                  child: Text(
+                    '₹$price',
+                  ),
+                ),
               ),
             ),
           ),
